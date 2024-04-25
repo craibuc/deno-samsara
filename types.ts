7efef47caccc6930a1c261036555f848b50d0d83
Craig Buchanan <craig.buchanan@cogniza.com>
2024-04-25 15:43:35 +0000

    refactoring DriverUpdateType

--- types.ts
+++ types.ts
@@ -1,12 +1,12 @@
-export type DriverType = {
-  name: string;
-  username: string;
-  password: string;
+export type DriverUpdateType = {
+  name?: string;
+  username?: string;
+  password?: string;
   driverActivationStatus?: "active" | "deactivated";
   phone?: string;
   licenseNumber?: number;
-  licenseState: string;
-  externalIds: {
+  licenseState?: string;
+  externalIds?: {
     [key: string]: string;
   };
   notes?: string;
